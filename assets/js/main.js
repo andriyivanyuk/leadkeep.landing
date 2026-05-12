2c1298e91e3d95bc3b47a07cb31ce899a5fff5af
andriyivanyuk <andriyivvanyuk@gmail.com>
2026-05-12 17:26:34 +0300

    logo added

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,10 +20,10 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src = "assets/img/logo/logo-2.svg";
+      if (logo) logo.src = "assets/img/logo/logo-2-blue.svg";
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "assets/img/logo/logo-2.svg";
+      if (logo) logo.src = "assets/img/logo/logo-2.svg";
     }
 
     // show or hide the back-top-top button
